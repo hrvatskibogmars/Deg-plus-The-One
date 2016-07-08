a72df88e14c890ec475a50709401cc24f8e7c115
root <root@WordPress-on-1404.033016-259>
2016-07-08 16:14:40 -0400

    finish him

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'online-trznica');
+define('DB_NAME', 'onlinetrznica');
 
 /** MySQL database username */
 define('DB_USER', 'root');
