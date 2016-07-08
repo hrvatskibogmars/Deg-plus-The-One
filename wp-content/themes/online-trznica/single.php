29ff209445a6590d216f6e6d3b2682ea26c4ccaa
jewbre <jewbre18@gmail.com>
2016-07-08 20:30:30 +0200

    asdlfj

--- wp-content/themes/online-trznica/single.php
+++ wp-content/themes/online-trznica/single.php
@@ -2,7 +2,28 @@
 
 the_post();
 get_header();
-$postDetails = get_fields();
-get_partial('article', $postDetails, false , 'post-templates');
+
+?>
+    <main class="main p-home">
+        <nav class="navigation">
+            <a href="/registracija" class="btn btn-black">Registriraj se</a>
+        </nav>
+        <div class="cover">
+            <div class="cover__image">
+                <div class="cover__text">
+                    <h1><?=get_the_title()?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="single">
+            <p class="single__text">
+                <?=get_the_content()?>
+            </p>
+
+        </div>
+        <?php get_partial('layout/_footer'); ?>
+    </main>
+
+<?php
 
 get_footer();
