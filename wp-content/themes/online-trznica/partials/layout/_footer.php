0c9c74746ab0c69227fb2cc90e180d5ba8b47fcc
jewbre <jewbre18@gmail.com>
2016-07-08 20:23:11 +0200

    footer + heade

--- wp-content/themes/online-trznica/partials/layout/_footer.php
+++ wp-content/themes/online-trznica/partials/layout/_footer.php
@@ -1,2 +1,8 @@
-
+<footer>
+    <p>
+        GKP Komunalac d.o.o. Mosna 15, 48000 Koprivnica<br>
+        Sva prava pridržana GKP Komunalac d.o.o. © 2016.<br>
+        Tel +385 (0)48 251 833, Fax +385 (0)48 251 898,<br>
+    </p>
+</footer>
     <?php wp_footer(); ?>
--- wp-content/themes/online-trznica/partials/layout/_head.php
+++ wp-content/themes/online-trznica/partials/layout/_head.php
@@ -9,4 +9,12 @@
           rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?=bu("css/main.css")?>">
     <title>Online tržnica</title>
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=bu("ui/favicon/apple-touch-icon.png")?>">
+    <link rel="icon" type="image/png" href="<?=bu("ui/favicon/favicon-32x32.png")?>" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?=bu("ui/favicon/favicon-16x16.png")?>" sizes="16x16">
+    <link rel="manifest" href="<?=bu("/manifest.json")?>">
+    <link rel="mask-icon" href="<?=bu("ui/favicon/safari-pinned-tab.svg")?>" color="#5bbad5 ">
+    <meta name="theme-color" content="#ffffff ">
 </head>
